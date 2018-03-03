8a5de4e1b8885ea626367f988c054f085becaddc
Ryan Switzer <rjs4244@rit.edu>
2018-03-03 03:15:23 -0500

    Implemented basic convolution

--- ced.py
+++ ced.py
@@ -11,16 +11,46 @@ import math
 from scipy.signal import convolve2d
 
 
-def ritconv():
-    # TODO: Implement me
+def ritconv(rows,cols,kr,kc,img,out):
+    '''
+    Basic 2D convolution
+    @params:
+        rows: number of horizontal pixels
+        cols: number of vertical pixels
+        kr: kernal row dimension
+        kc: kernel column dimension
+    @return:
+
+    '''
+    # TODO consider faster algorithm
+
+    kCenterX = kc/2
+    kCenterY = kr/2
+
+    for i in range(rows):
+        for j in range(cols):
+            #Kernel
+            for ki in range(kr):
+                fki = kRows - 1 - ki
+                for kj in range(kc):
+                    fkj = kCols - 1 - kj
+
+                    sig_i = i + (ki-kCenterY)
+                    sig_j = j + (kj+kCenterX)
+
+                    if (sig_i >= 0) and (sig_i < rows) \
+                        (sig_j >= 0) and (sig_j < cols):
+                        out[i][j] += img[sig_i][sig_j] * kernel[ki][kj]
 
 
 def ritgrad():
     # TODO Implement me
+    return None
 
 
 def ritcan():
     # TODO Implement me
+    return None
 
 
 def main():
@@ -28,3 +58,5 @@ def main():
         print('Need target file') # TODO Do we need to process multiple imgs?
         sys.exit()
     img = sys.argv[1]
+
+main()
