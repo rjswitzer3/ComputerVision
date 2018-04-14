7a254ec01c404400d2974547faec70f634409ae3
Ryan Switzer <rjs4244@rit.edu>
2018-04-14 13:29:30 -0400

    Completed kmeans implementation, lacking class comments

--- seg.py
+++ seg.py
@@ -17,6 +17,38 @@ EXTENSIONS = set(['jpg','jpeg','jif','jfif','jp2','jpx','j2k','j2c','fpx', \
                   'cgm','svg','gif'])
 #Path where the image shall exist
 PATH = 'images/'
+#Set of colorspace features
+FEATURES = {'HSV' : [0,255], 'GRAY' : [0,255]}
+#Threshold for tight cluster centroid
+THRESHOLD = 1.0
+
+class Centroid(object):
+    def __init__(self,value,id):
+        self.value = value
+        self.id = id
+    def str(self):
+        return str(self.id)+" - "+str(self.value)
+    def get_id(self):
+        return self.id
+    def get_value(self):
+        return self.value
+    def update(self,value):
+        self.value = value
+
+class Cluster(object):
+    def __init__(self,values,mean,centroid):
+        self.values = values
+        self.mean = mean
+        self.centroid = centroid
+    def append(self,value):
+        self.values.append(value)
+    def get_center(self):
+        return self.centroid
+    def get_values(self):
+        return self.values
+    def get_mean(self):
+        return np.mean(self.values)
+
 
 
 def write_result(imgs,img,name):
@@ -39,11 +71,43 @@ def write_result(imgs,img,name):
         cv.imwrite(newfile, img)
 
 
+def calc_error(centroids, cache):
+    '''
+    Calculate the average step distance from the prior centroid to the current.
+    '''
+    t_error = 0.0
+    for i in range(len(centroids)):
+        t_error += euclidean(centroids[i].get_value(),cache[i],None)
+    err = t_error/len(centroids)
+    print(err)
+    return err
+
+
+def label(pixel, centroids):
+    '''
+    Determine the label for the pixel by calculating the minimum difference to
+    each centroidself.
+    @params:
+        pixel: the pixel value at some position on a sample
+        centroids: list of all centroids
+    @returns:
+        id: the corresponding nearest centroid label
+    '''
+
+    distances = np.zeros(len(centroids))
+    for i in range(len(centroids)):
+        distances[i] = euclidean(pixel,centroids[i].get_value())
+    min = np.min(distances)
+    for c in centroids:
+        if min == euclidean(pixel, c.get_value()):
+            return c.get_id()
+
+
 def euclidean(a, b, ax=1):
     '''
     Calculate the euclidean distance between two points
     '''
-    return np.linalg.norm(a - b, axis=ax)
+    return np.linalg.norm(a - b)
 
 
 def loss(orig, xform):
@@ -65,13 +129,16 @@ def quantize(image, features, k):
     @params:
         image: the oringal input image
         features: deciphered features in the image
-        k: number of iterations to performed
+        k: number of clusters
     @returns:
         img: 2D array of cluster IDs with same dimensions as the input image
         cmap: array of cluster IDs corresponding cluster IDs to mean intensities
     '''
-    #TODO Implement
-    print('Starting quantization...')
+    samples = image.flatten() #TODO - Float32
+    #s = image.reshape((image.shape[0]*image.shape[1], 1))
+
+    clusters = kmeans(samples,k)
+
 
 
 def kmeans(samples, k):
@@ -83,35 +150,32 @@ def kmeans(samples, k):
     @returns:
         cluster: array of cluster assignment
     '''
-    #y,x,ch = samples.shape
-    #s = np.array(list(zip(samples)))
-    #s = samples.reshape(-1,2)[:,0]
-    s = samples#.reshape((samples.shape[0]))
-    cx = np.random.randint(0, np.max(s)-20, size=k)
-    cy = np.random.randint(0, np.max(s)-20, size=k)
-    cent = np.array(list(zip(cx,cy)))
+    s = samples
+    labels = [None]*len(s)
+    centroids, clusters, cache = ([] for i in range(3)) # values change based on colorsapce
 
-    cache = np.zeros(cent.shape)
-    clusters = np.zeros(len(s))
-    error = euclidean(cent,cache,None)
+    for i in range(k):
+        c = Centroid(np.random.randint(0,255),i)
+        centroids.append(c)
+        clusters.append(Cluster([],0.0,c))
+        cache.append(0.0)
+        print(c.str())
 
-    while error != 0:
+    error = calc_error(centroids,cache)
+
+    while error > THRESHOLD:
         for i in range(len(s)):
-            dist = euclidean(s[i],cent)
-            cluster = np.argmin(dist)
-            clusters[i] = cluster
-        cache = cent
-
+            labels[i] = label(s[i],centroids)
+            clusters[labels[i]].append(s[i])
+        #for i in range(k):
+        #    cache[i] = centroids[i].get_value()
+        cache = [centroids[i].get_value() for i in range(k)]
         for i in range(k):
-            pts = [s[j] for j in range(len(s)) if clusters[j] == i]
-            cent[i] = np.mean(pts, axis=0)
-        error = euclidean(cent, cache, None)
+            centroids[i].update(clusters[i].get_mean())
+            clusters[i] = Cluster([],0.0,centroids[i])
+        error = calc_error(centroids,cache)
 
-
-    #print(cx,cy)
-    #print(cent)
-    #print(cent[1][0])
-
+    return labels
 
 
 def main():
@@ -131,8 +195,9 @@ def main():
         global PATH
         PATH = PATH
         img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
-        fv = img.reshape((img.shape[0] * img.shape[1],3))
-        kmeans(fv, 3)
+        #fv = img.reshape((img.shape[0] * img.shape[1],3))
+        quantize(img, '', 3)
+
     else:
         print('Error unsupported input - ' + str(path))
         sys.exit()
